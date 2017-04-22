36ede6df7c4c19fdd07925559be798f3484695a3
Jackson Walker <cheezcakep@gmail.com>
2017-04-21 23:12:08 -0500

    it Works!
    aaa

--- WalkerBot.py
+++ WalkerBot.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-import tweepy, time, sys, keys.py
+import tweepy, time, sys, keys
 #4 turns 3 damage each
-auth = tweepy.0AuthHandler(keys.CONSUMER_KEY, keys.CONSUMER_SECRET)
+auth = tweepy.OAuthHandler(keys.CONSUMER_KEY, keys.CONSUMER_SECRET)
 auth.set_access_token(keys.ACCESS_TOKEN, keys.ACCESS_SECRET)
 api = tweepy.API(auth)
 
-api.update_status("damn what a test")
+api.update_status("application test")
