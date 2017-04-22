d982baebc41901ccaf3f2785c0f3b69d7682d43b
Jackson Walker <cheezcakep@gmail.com>
2017-04-21 23:08:31 -0500

    added a helloworld test
    ree

--- WalkerBot.py
+++ WalkerBot.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
-import tweepy, time, sys
+import tweepy, time, sys, keys.py
+#4 turns 3 damage each
+auth = tweepy.0AuthHandler(keys.CONSUMER_KEY, keys.CONSUMER_SECRET)
+auth.set_access_token(keys.ACCESS_TOKEN, keys.ACCESS_SECRET)
+api = tweepy.API(auth)
+
+api.update_status("damn what a test")
